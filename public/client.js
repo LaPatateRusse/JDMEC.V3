0da89592a8358afda937eac84ad4dccfde4d9a6a
LaPatateRusse <87035175+LaPatateRusse@users.noreply.github.com>
2025-01-28 22:43:57 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2,7 +2,7 @@
 const socket = io(
     window.location.hostname === "localhost"
         ? "http://localhost:3000"
-        : "https://jdmec-v3.onrender.com"
+        : "https://jdmec-v2.onrender.com"
 );
 
 // ✅ Récupération des éléments HTML
